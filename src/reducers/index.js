d24ca936f08d2c4d3cd3ca98f1888b6fa73b29a8
Jamal <moulayjam@gmail.com>
2020-04-11 17:57:58 -0400

    Reducers Setup

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+import { selectSong } from "../actions";
+
+const songsReducer =()=>{
+    return [
+        {title: 'No Scrubs', duration: '4:05'},
+        {title: 'My Lady', duration: '2:50'},
+        {title: 'Morning Light', duration: '3:25'},
+        {title: 'See You', duration: '5:07'}
+    ];
+}
+
+const selectedSongReducer =(selectedSong=null, action)=>{
+    if(action.type === 'SONGSELECTED'){
+        return action.payload;
+    }
+    return selectedSong
+};
